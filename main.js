7b2c5570e4184879a8edcb0c327f8fdce7e745bb
felipe cook <felipepcook@gmail.com>
2022-05-02 15:09:45 -0400

    used .toggle method on classlist to change display of element on html

--- main.js
+++ main.js
@@ -2,4 +2,17 @@
 
 window.addEventListener('scroll', () => {
   document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+});
+
+
+// Show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+  faq.addEventListener('click', () =>{
+    faq.classList.toggle('open'); 
+  })
 })
+
+
