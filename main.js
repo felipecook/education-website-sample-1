1f4ec58f7793dc21fb170bd3613d98a0d189515c
felipe cook <felipepcook@gmail.com>
2022-05-03 11:24:55 -0400

    completed Swiper JS

--- main.js
+++ main.js
@@ -26,3 +26,12 @@ faqs.forEach(faq => {
 })
 
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
